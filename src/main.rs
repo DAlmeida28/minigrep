98b24493ce7b463898201d08c0b3c19c450ae806
Daniel Almeida <almeida.danny@gmail.com>
2025-05-13 21:00:42 -0500

    add tests and refactoring code. add search and mofified run func. add case insensitive search

--- src/main.rs
+++ src/main.rs
@@ -6,12 +6,9 @@ fn main() {
     
     let config = Config::build(&args).unwrap_or_else(|err|
     {
-        println!("Problme parsing arguments: {err}");
+        println!("Problem parsing arguments: {err}");
         process::exit(1);
     });
-
-    println!("Searching for {}", config.query);
-    println!("In file {}", config.file_path);
     
     if let Err(e) = minigrep::run(config) {
         println!("Application error: {e}");
